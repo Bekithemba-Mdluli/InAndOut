3dee295dccbd92b2eedcc29977893a3a62031719
Bekithemba Mdluli <bekithemba.mdluli.bm@gmail.com>
2021-04-07 10:25:06 +0200

    final commit

---
+++ InAndOut/Controllers/ExpenseVm.cs
@@ -0,0 +1,12 @@
+﻿using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+
+namespace InAndOut.Controllers
+{
+    internal class ExpenseVm
+    {
+        public Expense Expense { get; internal set; }
+        public IQueryable<SelectListItem> TypeDropDown { get; internal set; }
+    }
+}
